c2b18fb160f33cfc568fccde4c6d3a04f927e31b
Webster Moswa <webroy94@gmail.com>
2020-05-19 17:55:33 +0200

    final commit

---
+++ app/src/main/java/com/moswag/jigsawpuzzle/cutter/HorizontalCurve.java
@@ -0,0 +1,13 @@
+package com.moswag.jigsawpuzzle.cutter;
+
+public enum HorizontalCurve {
+    NONE,
+    /**
+     * Curve head faced up
+     */
+    TAB,
+    /**
+     * Curve head faced down
+     */
+    SLOT
+}
---
+++ app/src/main/java/com/moswag/jigsawpuzzle/cutter/VerticalCurve.java
@@ -0,0 +1,16 @@
+package com.moswag.jigsawpuzzle.cutter;
+
+/**
+ * @author Vladimir Shevchenko vladimir@pantrylabs.com 05/04/16.
+ */
+public enum VerticalCurve {
+    NONE,
+    /**
+     * Curve head faced left
+     */
+    TAB,
+    /**
+     * Curve head faced right
+     */
+    SLOT
+}
